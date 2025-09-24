1346d0184d9bfc1e6c16cffae797cdc4f399bc55
matthew morton <mor21018@byui.edu>
2025-09-24 12:13:46 -0600

    Prep4 complete

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        List<int> numbers = new List<int>();
+
+        int number;
+        do
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            
+            numbers.Add(number);
+
+
+        } while (number != 0);
+
+        numbers.Remove(0);
+
+        Console.WriteLine($"the sum is: {numbers.Sum()}");
+        Console.WriteLine($"the average is: {numbers.Average()}");
+        Console.WriteLine($"the largest number is: {numbers.Max()}");
+
     }
 }
