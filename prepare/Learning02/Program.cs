d8a11a15f1bf9b7912714743dff6164a8b952058
matthew morton <mor21018@byui.edu>
2025-09-30 19:05:25 -0600

    learning 02 complete

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Apple";
+        job1._startYear = 2020;
+        job1._endYear = 2024;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Developer";
+        job2._company = "Google";
+        job2._startYear = 2025;
+        job2._endYear = 2029;
+
+
+        // job1.Display();
+        // job2.Display();
+
+        Resume myResume = new Resume();
+        myResume._name = "matthew";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        // Console.WriteLine($"{myResume._jobs[0]._jobTitle}");
+
+        myResume.Display();
+
     }
 }
