e7a18226adace6158d0e22154da21a27e6031cd6
matthew morton <mor21018@byui.edu>
2025-12-06 22:54:04 -0700

    Final Project foundation

---
+++ final/FinalProject/Dice.cs
@@ -0,0 +1,45 @@
+using System.Diagnostics.CodeAnalysis;
+
+class Dice
+{
+    // Attributes
+    private int _sides;
+    private int _quantity;
+    private int _modifier;
+    private bool _manual;
+
+    // Constructors
+public Dice(int sides, int quantity, int modifier, bool manual)
+    {
+        _sides = sides;
+        _quantity = quantity;
+        _modifier = modifier;
+        _manual = manual;
+    }
+
+
+public int RollDice()
+    {
+        int roll;
+        if (_manual)
+        {
+            Console.Write($"roll {_quantity}d{_sides}");
+            if(_modifier > 0)
+            {
+                Console.WriteLine($" and add {_modifier}");
+            }
+            else if(_modifier < 0)
+            {
+                Console.WriteLine($" and subtract {_modifier}");
+            }
+            Console.WriteLine("What did you roll?");
+            roll = int.Parse(Console.ReadLine());
+        }
+        else 
+        {
+            Random random = new Random();
+            roll = random.Next(_quantity,_quantity*_sides) + _modifier;
+        }
+        return roll;
+    }
+}
--- final/FinalProject/Program.cs
+++ final/FinalProject/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
+        Testing();
+
+
+
+
+        void Testing()
+        {
         Console.WriteLine("Hello FinalProject World!");
+        Dice d1 = new Dice(6 ,3,0,false);
+        Dice d2 = new Dice(6,3,0,false);
+        Dice d3 = new Dice(6,3,0,false);
+        Dice d4 = new Dice(6,3,0,false);
+        
+
+        d1.RollDice();
+        d2.RollDice();
+        d3.RollDice();
+        d4.RollDice();
+        }
     }
 }
